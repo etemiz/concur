581dbfed10f47aaf913be678091288ad6ec51be7
unknown <m.rafeh77@gmail.com>
2024-10-23 12:48:21 +0500

    golden text styling change

--- app/components/GoldenText.js
+++ app/components/GoldenText.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const GoldenText = ({ children }) => {
-  return <p class="golden-base golden1">{children}</p>;
+  return <p class="bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 bg-clip-text text-transparent">{children}</p>;
 };
 
 export default GoldenText;
